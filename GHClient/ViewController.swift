7217ba15f5ec4044d162b414ce9edb8e0db6d90a
Xin Guo <emagrorrimxguo@gmail.com>
2018-10-28 14:13:47 +0800

    Project init

---
+++ GHClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ghclient-ios
+//
+//  Created by Xin Guo  on 2018/10/28.
+//  Copyright © 2018 com.thoughtworks.workshop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
